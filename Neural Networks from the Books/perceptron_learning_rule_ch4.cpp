ba6909efedf5e458be35d7f07157a3e1c1dab75a
WilliamLewww <WilliamLew32@gmail.com>
2017-01-26 17:09:12 -0800

    completed single neuron learning rule

--- Neural Networks from the Books/perceptron_learning_rule_ch4.cpp
+++ Neural Networks from the Books/perceptron_learning_rule_ch4.cpp
@@ -1,11 +1,22 @@
 #include "perceptron_learning_rule_ch4.h"
 
 double* PerceptronAND::Run() {
-	//InitializeWeight();
-	bool maxLearning = false, cycle = false;
+	srand(time(0));
 
-	FeedForward(0);
-	std::cout << weight[0] << ":" << weight[1] << std::endl;
+	InitializeWeight();
+	bool maxLearning = false;
+
+	int iteration = 0, cycleCount = 0;
+	while (maxLearning == false) {
+		//std::cout << weight[0] << ":" << weight[1] << std::endl;
+		if (FeedForward(iteration)) { cycleCount += 1; }
+		else { cycleCount = 0; }
+
+		if (iteration == 2) { iteration = 0; }
+		else { iteration += 1; }
+
+		if (cycleCount == 3) maxLearning = true;
+	}
 
 	return weight;
 }
@@ -31,6 +42,11 @@ void PerceptronAND::LearningRule(int index, int value) {
 	if (input[index][2] == 0 && value == 1) {
 		for (int x = 0; x < 2; x++) { weight[x] -= input[index][x]; }
 	}
+}
+
+int PerceptronAND::hardlim(double value) {
+	if (value >= 0) { return 1; }
+	return 0;
 }
 
 void PerceptronAND::InitializeWeight() {
--- Neural Networks from the Books/perceptron_learning_rule_ch4.h
+++ Neural Networks from the Books/perceptron_learning_rule_ch4.h
@@ -1,18 +1,19 @@
 #pragma once
 #include <stdlib.h>
 #include <iostream>
+#include <time.h>
 
 class PerceptronAND {
 	double input[3][3] = {{  1,  2, 1 },
 						  { -1,  2, 0 },
 	                      {  0, -1, 0 }};
 
-	double weight[2] = {1.0, -0.8};
+	double weight[2] = { 1.0, -0.8 };
 
 	void InitializeWeight();
 	bool FeedForward(int index);
 	void LearningRule(int index, int value);
-	inline double hardlim(int value) { if (value >= 0) { return 1; } return 0; }
+	int hardlim(double value);
 public:
 	double* Run();
 };
