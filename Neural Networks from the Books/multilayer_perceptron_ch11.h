a64cb7913ebccb980f1cae0b87a43e5753302ef6
WilliamLewww <WilliamLew32@gmail.com>
2017-01-29 18:34:20 -0800

    fixed FeedForward function in Multi-Layer Perceptron

--- Neural Networks from the Books/multilayer_perceptron_ch11.h
+++ Neural Networks from the Books/multilayer_perceptron_ch11.h
@@ -13,7 +13,7 @@ class PerceptronML {
 	std::vector<double> b1 = { -0.48, -0.13 };
 
 	std::vector<std::vector<double>> w2 = {{ 0.09, -0.17 }};
-	std::vector<std::vector<double>> b2 = {{ 0.48 }};
+	std::vector<double> b2 = { 0.48 };
 
 	std::vector<std::vector<double>> FeedForward(std::vector<std::vector<double>> input, std::vector<std::vector<double>> weight, std::vector<double> bias, int function);
 	double Activation(int function, double value);
--- Neural Networks from the Books/multilayer_perceptron_ch11.h.cpp
+++ Neural Networks from the Books/multilayer_perceptron_ch11.h.cpp
@@ -1,26 +1,30 @@
 #include "multilayer_perceptron_ch11.h"
 
 void PerceptronML::Run() {
-	FeedForward(a0, w1, b1, 1);
+	std::vector<std::vector<double>> a1 = FeedForward(a0, w1, b1, 1);
+	FeedForward(a1, w2, b2, 0);
 }
 
 std::vector<std::vector<double>> PerceptronML::FeedForward(std::vector<std::vector<double>> input, std::vector<std::vector<double>> weight, std::vector<double> bias, int function) {
 	std::vector<std::vector<double>> product;
 	std::vector<double> row;
 
-	double total;
+	double total = 0;;
 	for (int y = 0; y < weight.size(); y++) {
 		for (int x = 0; x < input[0].size(); x++) {
 			for (int inner = 0; inner < weight[0].size(); inner++) {
-				total = Activation(function, (weight[y][inner] * input[inner][x]) + bias[y]);
-				row.push_back(total);
+				total += weight[y][inner] * input[inner][x];
 			}
+
+			total = Activation(function, total + bias[y]);
+			row.push_back(total);
+			total = 0;
 		}
 		product.push_back(row);
 		row.clear();
 	}
 
-	return weight;
+	return product;
 }
 
 double PerceptronML::Activation(int function, double value) {
